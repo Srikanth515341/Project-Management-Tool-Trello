b6f9c0b2e5263d380fb155093b4b000894576db8
Srikanth Nelluri <nellurisrikanthchowdary007@gmail.com>
2025-02-20 12:08:50 +0100

    Added Express server and MongoDB connection

---
+++ server/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/project-management", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Connection Error:", err));
+
+// Simple Route
+app.get("/", (req, res) => {
+  res.send("Project Management Tool API is running...");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
